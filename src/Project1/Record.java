e1f1eaea1112055e0f6af2b6e0bdfe92611b80ce
MikeRosin <107156239+MikeRosin@users.noreply.github.com>
2022-06-22 17:08:07 -0400

    Update Record.java

--- src/Project1/Record.java
+++ src/Project1/Record.java
@@ -2,6 +2,46 @@ package Project1;
 import java.util.ArrayList;
 
 public class Record {
+    private Date;
+    private String Course;
+    private String Student;
+    private float grade;
 
 
+    public Record(Date, Student, Course, grade){
+
+    }
+
+    public String getDate(){
+        return Date;
+    }
+
+    public void setDate(date : Date){
+        this.date = date;
+    }
+
+    public Student getStudent(){
+        return Student;
+    }
+
+    public void setStudent(student : Student){
+        this.student = student;
+    }
+
+    public Course getCourse(){
+        return Course;
+    }
+
+    public void setCourse(course : Course) {
+        this.Course = Course;
+    }
+
+    public float getGrade(){
+        return grade;
+    }
+
+    public void setGrade(grade : Grade) {
+        this.grade = grade;
+    }
 }
+
